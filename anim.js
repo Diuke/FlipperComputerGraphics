c8a18d764f65ddee97f1cd2483aea3b8726ae7c2
Diuke <juan-pablo-duque@hotmail.com>
2020-06-27 18:18:25 +0200

    initial commit

---
+++ anim.js
@@ -0,0 +1,46 @@
+function anim() {
+	/*
+	duration in time (seconds)
+	starting point
+	control point for the start
+	control point for the end 
+	ending point
+	
+	each point has:
+		vector orientation [3]
+		quaternion orentation
+	*/
+	return [
+ [2, [0,0,0],   Quaternion.fromAxisAngle([1,0,0],utils.degToRad(0)),
+ 	 [8,0,0],   Quaternion.fromAxisAngle([0,1,0],utils.degToRad(0)),
+ 	 [8,0,0],   Quaternion.fromAxisAngle([0,1,0],utils.degToRad(90)),
+ 	 [8,0,0],   Quaternion.fromAxisAngle([0,1,0],utils.degToRad(90))],
+	 
+ [2, [8,0,0],   Quaternion.fromAxisAngle([0,1,0],utils.degToRad(90)),
+	 [8,0,-8],   Quaternion.fromAxisAngle([0,1,0],utils.degToRad(90)),
+ 	 [8,0,-8],  Quaternion.fromAxisAngle([0,0,1],utils.degToRad(90)),
+ 	 [8,0,-8],  Quaternion.fromAxisAngle([0,0,1],utils.degToRad(90))],
+	 
+ [2, [8,0,-8],   Quaternion.fromAxisAngle([0,0,1],utils.degToRad(90)),
+	 [8,0,-8],   Quaternion.fromAxisAngle([0,0,1],utils.degToRad(90)),
+ 	 [8,8,-8],   Quaternion.fromAxisAngle([0,1,0],utils.degToRad(180)),
+ 	 [8,8,-8],   Quaternion.fromAxisAngle([0,1,0],utils.degToRad(180))],
+	 
+ [2, [8,8,-8],   Quaternion.fromAxisAngle([0,1,0],utils.degToRad(180)),
+	 [8,8,-8],   Quaternion.fromAxisAngle([0,1,0],utils.degToRad(180)),
+ 	 [0,8,-8],   Quaternion.fromAxisAngle([0,1,0],utils.degToRad(270)),
+ 	 [0,8,-8],   Quaternion.fromAxisAngle([0,1,0],utils.degToRad(270))],
+	 
+ [2, [0,8,-8],   Quaternion.fromAxisAngle([0,1,0],utils.degToRad(270)),
+	 [0,8,-8],   Quaternion.fromAxisAngle([0,1,0],utils.degToRad(270)),
+ 	 [0,8,0],   Quaternion.fromAxisAngle([0,0,1],utils.degToRad(-90)),
+ 	 [0,8,0],   Quaternion.fromAxisAngle([0,0,1],utils.degToRad(-90))],
+	 
+	 
+ [2, [0,8,0],   Quaternion.fromAxisAngle([0,0,1],utils.degToRad(-90)),
+	 [0,8,0],   Quaternion.fromAxisAngle([0,0,1],utils.degToRad(-90)),
+ 	 [0,0,0],   Quaternion.fromAxisAngle([1,0,0],utils.degToRad(0)),
+ 	 [0,0,0],   Quaternion.fromAxisAngle([1,0,0],utils.degToRad(0))]
+];
+}
+
